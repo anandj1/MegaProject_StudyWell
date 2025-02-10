2eaa2e31d50e50b53d6aa626ed1c18e930b16e59
Anand <131908427+anandj1@users.noreply.github.com>
2025-02-10 11:34:49 +0530

    Update index.js

--- Backkend/index.js
+++ Backkend/index.js
@@ -30,7 +30,7 @@ app.use(
       "http://localhost:3000"
     ],
     credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE"],
+    methods: ["GET", "POST", "PUT", "DELETE","OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"]
   })
 )
