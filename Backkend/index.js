ec0228218dde98edcd4144f5bbbd9fa5e7d84f2f
Anand <131908427+anandj1@users.noreply.github.com>
2025-02-10 11:32:43 +0530

    Update index.js

--- Backkend/index.js
+++ Backkend/index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-
+const path = require("path"); 
 const userRoutes = require("./routes/User");
 const profileRoutes = require("./routes/Profile");
 const paymentRoutes = require("./routes/Payments");
